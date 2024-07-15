8504144abcb2f5f7a80ff14d1a53794b5fad05ba
Mohamed Elsayed <mohamedelsayed652002@gmail.com>
2024-07-16 02:13:05 +0300

    .

--- filtered_logger.py
+++ filtered_logger.py
@@ -17,24 +17,6 @@ def filter_datum(fields: List[str], redaction: str,
     return re.sub(pattern, fr'\g<field>={redaction}', message)
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        """intitializing the class instances"""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """a method for overriding the parent class format method"""
-        message = super(RedactingFormatter, self).format(record)
-        return filter_datum(self.fields, self.REDACTION,
-                            message, self.SEPARATOR)
 def get_logger() -> logging.Logger:
     """returns a logger object"""
     logger = logging.getLogger('user_data')
@@ -80,3 +62,27 @@ def main() -> None:
             args = ("user_data", logging.INFO, None, None, message, None, None)
             log_record = logging.LogRecord(*args)
             logger.handle(log_record)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """intitializing the class instances"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """a method for overriding the parent class format method"""
+        message = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            message, self.SEPARATOR)
+
+
+if __name__ == "__main__":
+    main()
